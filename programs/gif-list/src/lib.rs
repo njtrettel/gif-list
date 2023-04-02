1e5d39eba0ab442c3e3665456bff252cbab6cb32
Nicolas Trettel <njtret0815@gmail.com>
2023-04-02 18:54:52 -0400

    add gif list user code and tests

--- programs/gif-list/src/lib.rs
+++ programs/gif-list/src/lib.rs
@@ -1,15 +1,53 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7hfWPsdmi52vhUTpcMGXGt6sXnrYorCuc5Pxj61mNNML");
 
 #[program]
 pub mod gif_list {
-    use super::*;
+  use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+  pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    let account = &mut ctx.accounts.allowed_gifs;
+    account.gifs = vec![];
+    account.bump = *ctx.bumps.get("allowed_gifs").unwrap();
+    Ok(())
+  }
+
+  // pub fn request_access_to_gif(ctx: Context<RequestAccess>) -> Result<()> {
+  //   Ok(())
+  // }
 }
 
+/* ACCOUNTS */
+
+#[account]
+pub struct AllowedGifs {
+  pub gifs: Vec<String>,
+  pub bump: u8
+}
+
+/* INSTRUCTION VALIDATION */
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+  #[account(mut)]
+  pub authority: Signer<'info>,
+  #[account(init,
+    payer = authority,
+    seeds = [b"allowed_gifs", authority.key().as_ref()],
+    space = 9000,
+    bump
+  )]
+  pub allowed_gifs: Account<'info, AllowedGifs>,
+  pub system_program: Program<'info, System>
+}
+
+// #[derive(Accounts)]
+// pub struct RequestAccess<'info> {
+//   pub authority: Signer<'info>
+//   #[account(seeds = [b"allowed_gifs", authority.key().as_ref()], bump = allowed_gifs.bump)]
+//   pub allowed_gifs: Account<'info, AllowedGifs>
+//   pub system_program: Program<'info, System>
+// }
+
+// impl<'info> RequestAccess
--- tests/gif-list.ts
+++ tests/gif-list.ts
@@ -1,16 +1,89 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { GifList } from "../target/types/gif_list";
+import { GifListAdmin } from "../target/types/gif_list_admin";
+
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
+
+const user = anchor.web3.Keypair.generate();
+
+const addFunds = async (wallet: anchor.web3.Keypair, amount: number) => {
+  const tx = await provider.connection.requestAirdrop(wallet.publicKey, amount);
+  const latestHash = await provider.connection.getLatestBlockhash();
+  await provider.connection.confirmTransaction({
+    blockhash: latestHash.blockhash,
+    lastValidBlockHeight: latestHash.lastValidBlockHeight,
+    signature: tx
+  });
+};
+
+const program = anchor.workspace.GifList as Program<GifList>;
+const [allowedGifsPda] = anchor.web3.PublicKey.findProgramAddressSync([
+  anchor.utils.bytes.utf8.encode('allowed_gifs'),
+  user.publicKey.toBuffer(),
+], program.programId);
+
+const adminProgram = anchor.workspace.GifListAdmin as Program<GifListAdmin>;
+const [gifListPda] = anchor.web3.PublicKey.findProgramAddressSync([
+  anchor.utils.bytes.utf8.encode('gif_list'),
+  provider.wallet.publicKey.toBuffer(),
+], adminProgram.programId);
+const [conditionsPda] = anchor.web3.PublicKey.findProgramAddressSync([
+  anchor.utils.bytes.utf8.encode('conditions'),
+  provider.wallet.publicKey.toBuffer(),
+], adminProgram.programId);
+
+describe("gif-list-admin", () => {
+  it("initializes", async () => {
+    const tx = await adminProgram.methods
+      .initialize(new anchor.BN(0))
+      .accounts({
+        authority: provider.wallet.publicKey,
+        gifList: gifListPda,
+        conditions: conditionsPda
+      })
+      .rpc();
+    console.log('Your transaction signature', tx);
+
+    const gifList = await adminProgram.account.gifList.fetch(gifListPda);
+    console.log('Gif list', gifList);
+  });
+
+  it('can add a new gif', async () => {
+    const tx = await adminProgram.methods
+      .addGif("https://media.giphy.com/media/MDJ9IbxxvDUQM/giphy.gif")
+      .accounts({
+        authority: provider.wallet.publicKey,
+        gifList: gifListPda
+      })
+      .rpc();
+    console.log('Your transaction signature', tx);
+    
+    const gifList = await adminProgram.account.gifList.fetch(gifListPda);
+    console.log('Gif list', gifList);
+  });
+});
 
 describe("gif-list", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
-  const program = anchor.workspace.GifList as Program<GifList>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("initializes", async () => {
+    await addFunds(user, anchor.web3.LAMPORTS_PER_SOL * 1);
+    console.log('ADDED FUNDS');
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        authority: user.publicKey,
+        allowedGifs: allowedGifsPda
+      })
+      .signers([user])
+      .rpc();
+    // await anchor.web3.sendAndConfirmTransaction(provider.connection, tx, [user]);
     console.log("Your transaction signature", tx);
+
+    const allowedGifs = await program.account.allowedGifs.fetch(allowedGifsPda);
+    const gifList = await adminProgram.account.gifList.fetch(gifListPda);
+    console.log('Allowed gifs: ', allowedGifs);
+    console.log('Gif list: ', gifList);
+    
   });
 });
